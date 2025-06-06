d0c67257ce8088db490e1a85ded9002b99ffc37b
Vaishnavi <lakshmivaishnavi137@gmail.com>
2025-06-06 15:43:29 +0530

    commit

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  // Use the environment variable here.
-  // In local development, it will get the value from .env.local.
-  // On Vercel, it will get the value from the Vercel project's environment variables.
-  baseURL: import.meta.env.VITE_APP_BACKEND_URL || 'http://localhost:5000/api',
+
+  baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000/api',
   withCredentials: true,
 });
 
