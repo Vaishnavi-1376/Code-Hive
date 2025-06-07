5c8328f84e583f7c91796fa5fd1af5cd3e108da2
Vaishnavi <lakshmivaishnavi137@gmail.com>
2025-06-07 22:06:40 +0530

    commit

--- backend/main-backend/models/User.js
+++ backend/main-backend/models/User.js
@@ -19,6 +19,7 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
+
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password); 
 };
